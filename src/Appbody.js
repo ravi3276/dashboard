3987c9be9a09f5b43e0b6789a0df214d3a3a811c
raviteja3276 <ravitejam3276@gmail.com>
2021-04-04 01:55:18 +0530

    dashboard

--- src/Appbody.js
+++ src/Appbody.js
@@ -7,6 +7,10 @@ import EuroSymbolIcon from '@material-ui/icons/EuroSymbol';
 import EventIcon from '@material-ui/icons/Event';
 import Profile from './Profile';
 import { Avatar } from '@material-ui/core';
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import CallMadeIcon from '@material-ui/icons/CallMade';
+import CallReceivedIcon from '@material-ui/icons/CallReceived';
 
 function Appbody() {
     return (
@@ -124,7 +128,126 @@ function Appbody() {
                 </div>
                 </div>
 
+                <div className="appbody__footer">
+                <div className="appbody__footerleft">
+                <div className="footertitle">
+                <h4>PROPERTY INSIGHTS</h4>
+                
+                    <img src="https://resources.useready.com/wp-content/uploads/2020/08/Display-Top-Total-Donut-Chart-Tableau-1.png"
+                    alt="" />
+                </div>
+                    
+                    <div className="propertyfooter">
+                    <div className="icon">
+                    <input placeholder="Total"/>
+                    <ExpandMoreIcon />
+                    </div>
+                    <div className="appbody__footerleft1">
+                    <RadioButtonUncheckedIcon className="btn1"/>
+                    <h3>
+                    Property 1
+                    <small>120Bookings</small>
+                    </h3>
+                    </div>
+
+                    <div className="appbody__footerleft1">
+                    <RadioButtonUncheckedIcon className="btn2"/>
+                    <h3>
+                    Property 2
+                    <small>98Bookings</small>
+                    </h3>
+                    </div>
+
+                    <div className="appbody__footerleft1">
+                    <RadioButtonUncheckedIcon className="btn3"/>
+                    <h3>
+                    Property 3
+                    <small>90Bookings</small>
+                    </h3>
+                    </div>
+
+                    <div className="appbody__footerleft1">
+                    <RadioButtonUncheckedIcon className="btn4"/>
+                    <h3>
+                    Property 4
+                    <small>88Bookings</small>
+                    </h3>
+                    </div>
+
+                    </div>
+
+                    </div>
+
+                    <div className="appbody__footerright">
+                        <div className="dashboardfinal">
+                        <h3>PAYMENTS</h3>
+                        <p>See All(10)</p>
+                        </div>
+                    <div className="overall">
+                    <div className="dates">
+                        <p>
+                        08.02.2021
+                        </p>
+                        <small>10:30AM</small>
+                    </div>
+
+                    <div className="payment">
+                    <h4>Payment Received</h4>
+                    <p>New BookingId 'Propert1'</p>
+                    </div>
+
+                    <div className="amount">
+                    <p>1000.00</p>
+                    <CallReceivedIcon />
+                    </div>
+
+                    </div>
+
+                    <div className="overall">
+                    <div className="dates">
+                        <p>
+                        08.02.2021
+                        </p>
+                        <small>10:30AM</small>
+                    </div>
+
+                    <div className="payment">
+                    <h4>Payment Received</h4>
+                    <p>New BookingId 'Propert1'</p>
+                    </div>
+
+                    <div className="amount">
+                    <p>1000.00</p>
+                    <CallReceivedIcon />
+                    </div>
+
+                    </div>
+
+                    <div className="overall">
+                    <div className="dates">
+                        <p>
+                        08.02.2021
+                        </p>
+                        <small>10:30AM</small>
+                    </div>
+
+                    <div className="payment">
+                    <h4>Amount Refund</h4>
+                    <p>New BookingId 'Propert1'</p>
+                    </div>
+
+                    <div className="amount">
+                    <p>1000.00</p>
+                    <CallMadeIcon color="red" className="redarrow"/>
+                    </div>
+
+                    </div>
+
+
+                    </div>
+                </div>
            </div>
+
 
 
            </div>
